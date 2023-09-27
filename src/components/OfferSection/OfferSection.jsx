be06077792520c3e2113c29502953e564d742253
Mikhail Pryada <pryada16@gmail.com>
2023-09-27 10:27:36 +0300

    First section & 1/4 of second section is done

--- src/components/OfferSection/OfferSection.jsx
+++ src/components/OfferSection/OfferSection.jsx
@@ -1,9 +1,22 @@
 import "./OfferSection.css";
 
+
 const OfferSection = () => {
   return (
-    <div>
-      
+    <div className="offer-section">
+      <div className="background"></div>
+      <div className="glow-line"></div>
+      <div className="section-grid">
+        <div className="vito-container">
+          <div className="vito-backlight"></div>
+          <div className="vito"></div>
+        </div>
+        <div className="right-side-flexbox">
+          <h2></h2>
+          <table></table>
+          <figure></figure>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/PromoSection/PromoSection.jsx
+++ src/components/PromoSection/PromoSection.jsx
@@ -2,8 +2,23 @@ import "./PromoSection.css";
 
 const PromoSection = () => {
   return (
-    <div>
-
+    <div className="promo-section">
+      <div className="background"></div>
+      <div className="shading-left"></div>
+      <div className="shading-right"></div>
+      <div className="mafia-costume-female"></div>
+      <div className="mafia-costume-male"></div>
+      <div className="shading-bottom"></div>
+      <div className="glow-line"></div>
+      <div className="glow-center"></div>
+      <div className="logo"></div>
+      <div className="boxes"></div>
+      <div className="text-container">
+        <h1 className="header">КОЛЛЕКЦИОННЫЕ ИЗДАНИЯ</h1>
+        <p className="slogan">
+          Побеждайте легко, побеждайте стильно с новыми коллекционными изданиями «Аллодов Онлайн»!
+        </p>
+      </div>
     </div>
   );
 };
